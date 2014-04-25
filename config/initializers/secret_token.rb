d9c173ed85ef530a27057fdf8769c165abe9976d
aberracja <barbaraklosowska@gmail.com>
2014-04-25 13:32:07 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Books::Application.config.secret_key_base = 'da6c8529c574889df7e76caf913148bafed9280c6729aa267c5ca72d01a5dc15210fd7c580485c8f43d787f94ce081e30fc77cc8a62a4087636b9de9179425bd'
