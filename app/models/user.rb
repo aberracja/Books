73113d4b01f68588df96c56d49c5b0ad2ead6691
aberracja <barbaraklosowska@gmail.com>
2014-06-03 14:58:28 +0000

    sign in sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-#attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
+# TU COŚ NIE DZIAŁA:
+#  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
   
 end
